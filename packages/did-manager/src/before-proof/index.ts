527cf3961dc985f5529dc8893970426e624d0705
Mudlee <sandor.muranyi@iop-ventures.com>
2019-11-28 15:37:34 +0000

    Started to implement layer-2 state

---
+++ packages/did-manager/src/before-proof/index.ts
@@ -0,0 +1,48 @@
+import { TimeSeries } from "../time-series";
+
+export interface IBeforeProofOperations {
+  register(height: number): void;
+  revoke(height: number): void;
+}
+
+export interface IBeforeProofState {
+  readonly apply: IBeforeProofOperations;
+  readonly revert: IBeforeProofOperations;
+
+  existsAt(height: number): boolean;
+}
+
+export class BeforeProofState implements IBeforeProofState {
+
+  public readonly apply: IBeforeProofOperations = {
+    register: (height: number) => {
+      if (this.periods.get(height)) {
+        throw new Error(`Before proof ${this.contentId} is already registered at ${height}`);
+      }
+      this.periods.set(height, true);
+    },
+    revoke: (height: number) => {
+      if (this.periods.get(height)) {
+        throw new Error(`Before proof ${this.contentId} is not registered at ${height}`);
+      }
+      this.periods.set(height, false);
+    },
+  };
+
+  public readonly revert: IBeforeProofOperations = {
+    register: (height: number) => {
+      this.periods.unset(height, true);
+    },
+    revoke: (height: number) => {
+      this.periods.unset(height, false);
+    },
+  };
+
+  private periods = new TimeSeries(false);
+
+  public constructor(public readonly contentId: string) {}
+
+  public existsAt(height: number): boolean {
+    return this.periods.get(height);
+  }
+}
---
+++ packages/hydra-plugin/src/MorpheusTransactionHandler.ts
@@ -0,0 +1,43 @@
+import { Database, State } from "@arkecosystem/core-interfaces";
+import { Handlers, TransactionReader } from "@arkecosystem/core-transactions";
+import { Interfaces, Transactions, Utils } from "@arkecosystem/crypto";
+
+import { Transaction } from "@internet-of-people/did-manager";
+const { key, type, typeGroup } = Transaction.MorpheusTransaction;
+import { IMorpheusState } from "./MorpheusState";
+
+
+export class MorpheusTransactionHandler extends Handlers.TransactionHandler {
+  public constructor(private state: IMorpheusState) 
+  {
+    super();
+  }
+
+  public getConstructor(): typeof Transactions.Transaction {
+    return Transaction.MorpheusTransaction;
+  }
+
+  public dependencies(): readonly Handlers.TransactionHandlerConstructor[] {
+    return [];
+  }
+
+  public walletAttributes(): readonly string[] {
+    return [];
+  }
+
+  public bootstrap(connection: Database.IConnection, walletManager: State.IWalletManager): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async isActivated(): Promise<boolean> {
+    return true;
+  }
+
+  public async applyToRecipient(transaction: Interfaces.ITransaction, walletManager: State.IWalletManager): Promise<void> {
+    // nothing to do here
+  }
+
+  public async revertForRecipient(transaction: Interfaces.ITransaction, walletManager: State.IWalletManager): Promise<void> {
+    // nothing to do here
+  }
+}
