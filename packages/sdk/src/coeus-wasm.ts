42645493c7dfbc294375156e19fa27c367d00cba
Sandor Muranyi <contact@mudlee.hu>
2020-10-26 14:12:45 +0100

    sdk reexports wasm packages for Coeus

---
+++ packages/sdk/src/coeus-wasm.ts
@@ -0,0 +1,17 @@
+import {
+  CoeusTxBuilder,
+  DomainName,
+  HydraSigner,
+  NoncedOperationsBuilder,
+  UserOperation,
+  PrivateKey,
+} from '@internet-of-people/sdk-wasm';
+
+export {
+  CoeusTxBuilder,
+  DomainName,
+  HydraSigner,
+  NoncedOperationsBuilder,
+  UserOperation,
+  PrivateKey,
+};
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,12 +4,14 @@ import * as Authority from './authority';
 import * as Layer1 from './layer1';
 import * as Layer2 from './layer2';
 import * as Types from './types';
+import * as Coeus from './coeus-wasm';
 
 export * from './signed';
 export * from './network';
 export {
   Ark,
   Authority,
+  Coeus,
   Crypto,
   Layer1,
   Layer2,
