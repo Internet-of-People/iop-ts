d54c114ac98da522defde295979987cc1f2f7f9c
wigy <wigy_opensource_developer@yahoo.com>
2021-07-02 11:10:19 +0200

    Bumping version to 5.0.0-rc.1

--- scripts/publish/stable.sh
+++ scripts/publish/stable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a packages=("sdk-wasm" "morpheus-crypto" "hydra-plugin-core" "coeus-proto" "coeus-node" "did-manager" "sdk" "hydra-plugin")
+declare -a packages=("sdk-wasm" "node-wasm" "morpheus-crypto" "hydra-plugin-core" "coeus-proto" "coeus-node" "did-manager" "sdk" "hydra-plugin")
 
 npm config set registry https://registry.npmjs.org/
 
