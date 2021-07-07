b6d741ce3e36fa4e609e14d3d14307070a0d1dac
mudlee <contact@mudlee.hu>
2021-07-07 09:52:40 +0200

    Log hydra-plugin's version when it starts up

--- packages/hydra-plugin/src/main.ts
+++ packages/hydra-plugin/src/main.ts
@@ -22,6 +22,7 @@ import { Types } from '@internet-of-people/sdk';
 import { ArkConnector } from './ark-connector';
 import { BlockEventSource } from './block-event-source';
 import { schedule } from './scheduler';
+import { version } from '../package.json';
 
 const { MorpheusStateHandler: { MorpheusStateHandler } } = MorpheusTransaction;
 type TransactionId = Types.Sdk.TransactionId;
@@ -42,6 +43,7 @@ export class Composite implements IInitializable {
   }
 
   public async init(): Promise<void> {
+    this.log.info(`Version: ${version}`);
     this.log.info('Initializing');
 
     for (const component of this.subcomponents) {
