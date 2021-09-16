f3c8ad9e3d1b12f6961b938a68ecb358dcfe4587
wigy <wigy_opensource_developer@yahoo.com>
2021-09-16 18:28:36 +0200

    IMorpheusApi.getBeforeProofHistory might return a txid

--- packages/sdk/src/types/layer2/before-proof-history.ts
+++ packages/sdk/src/types/layer2/before-proof-history.ts
@@ -1,7 +1,9 @@
 import * as Sdk from '../sdk';
 
+// txid === undefined is possible to not break backward compatibility in Morpheus v1 API
 export interface IBeforeProofHistory {
   contentId: Sdk.ContentId;
   existsFromHeight: number | null;
+  txid?: Sdk.TransactionId | null;
   queriedAtHeight: number;
 }
